6594dfe90b274e6e772da83c4320eaa713699c03
WCCharles <wccharles@linux.com>
2022-04-09 18:30:16 +0100

    -wshadow flag for yoga include

--- react_juce/core/YogaImplInclude.cpp
+++ react_juce/core/YogaImplInclude.cpp
@@ -2,16 +2,16 @@
 
 // Disable compiler warnings for yoga
 #if _MSC_VER
-#pragma warning(push)
-  #pragma warning(disable : 4018) // signed/unsigned mismatch
-  #pragma warning(disable : 4127) // conditional expression is constant
-  #pragma warning(disable : 4244) // possible loss of data
-  #pragma warning(disable : 4505) // unreferenced local function
-  #pragma warning(disable : 4611) // object destruction is non-portable
-  #pragma warning(disable : 4702) // unreachable code
-  #pragma warning(disable : 4100 4244 4459)
+ #pragma warning(push)
+ #pragma warning(disable : 4018) // signed/unsigned mismatch
+ #pragma warning(disable : 4127) // conditional expression is constant
+ #pragma warning(disable : 4244) // possible loss of data
+ #pragma warning(disable : 4505) // unreferenced local function
+ #pragma warning(disable : 4611) // object destruction is non-portable
+ #pragma warning(disable : 4702) // unreachable code
+ #pragma warning(disable : 4100 4244 4459)
 #elif __clang__
-#pragma clang diagnostic push
+ #pragma clang diagnostic push
  #pragma clang diagnostic ignored "-Wextra-semi"
  #pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
  #pragma clang diagnostic ignored "-Wunused-parameter"
@@ -28,17 +28,16 @@
   #pragma clang diagnostic ignored "-Wconversion"
  #endif
 #elif __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wzero-as-null-pointer-constant"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wswitch-enum"
-#pragma GCC diagnostic ignored "-Wunused-variable"
-#pragma GCC diagnostic ignored "-Wredundant-decls"
-#pragma GCC diagnostic ignored "-Wpedantic"
+ #pragma GCC diagnostic push
+ #pragma GCC diagnostic ignored "-Wzero-as-null-pointer-constant"
+ #pragma GCC diagnostic ignored "-Wsign-conversion"
+ #pragma GCC diagnostic ignored "-Wshadow"
+ #pragma GCC diagnostic ignored "-Wswitch-enum"
+ #pragma GCC diagnostic ignored "-Wunused-variable"
+ #pragma GCC diagnostic ignored "-Wredundant-decls"
+ #pragma GCC diagnostic ignored "-Wpedantic"
 #endif
 
-#include "yoga/yoga/log.cpp"
-#include "yoga/yoga/event/event.cpp"
 #include "yoga/yoga/Utils.cpp"
 #include "yoga/yoga/YGConfig.cpp"
 #include "yoga/yoga/YGEnums.cpp"
@@ -48,6 +47,8 @@
 #include "yoga/yoga/YGStyle.cpp"
 #include "yoga/yoga/YGValue.cpp"
 #include "yoga/yoga/Yoga.cpp"
+#include "yoga/yoga/event/event.cpp"
+#include "yoga/yoga/log.cpp"
 
 #include "yoga/yoga/YGMacros.h"
 
@@ -59,12 +60,10 @@
 // type and everything seems to work ok.
 // See: https://github.com/facebook/yoga/issues/891
 #ifndef NS_ENUM
-#undef YG_ENUM_BEGIN
-#define YG_ENUM_BEGIN(name) enum name: unsigned
+ #undef YG_ENUM_BEGIN
+ #define YG_ENUM_BEGIN(name) enum name : unsigned
 #endif
 
-#include "yoga/yoga/log.h"
-#include "yoga/yoga/event/event.h"
 #include "yoga/yoga/YGConfig.h"
 #include "yoga/yoga/YGEnums.h"
 #include "yoga/yoga/YGFloatOptional.h"
@@ -74,12 +73,14 @@
 #include "yoga/yoga/YGValue.h"
 #include "yoga/yoga/Yoga-internal.h"
 #include "yoga/yoga/Yoga.h"
+#include "yoga/yoga/event/event.h"
+#include "yoga/yoga/log.h"
 
 // Enable compiler warnings
 #if _MSC_VER
-#pragma warning (pop)
+ #pragma warning(pop)
 #elif __clang__
-#pragma clang diagnostic pop
+ #pragma clang diagnostic pop
 #elif __GNUC__
-#pragma GCC diagnostic pop
+ #pragma GCC diagnostic pop
 #endif
