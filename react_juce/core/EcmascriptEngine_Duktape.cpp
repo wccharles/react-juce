53e980eb3b67acacfdce940de31b4328710f6297
WCCharles <wccharles@linux.com>
2022-04-10 16:23:32 +0100

    more compiler ignore warnings

--- react_juce/core/EcmascriptEngine_Duktape.cpp
+++ react_juce/core/EcmascriptEngine_Duktape.cpp
@@ -24,8 +24,13 @@
 
 #if _MSC_VER
 #pragma warning(push)
-#pragma warning ( disable : 4996 )
 #pragma warning ( disable : 4127 )
+#pragma warning ( disable : 4996 )
+#pragma warning ( disable : 4611 )
+#pragma warning ( disable : 4244 )
+#pragma warning ( disable : 4458 )
+#pragma warning ( disable : 4459 )
+#pragma warning ( disable : 4505 )
 #elif __clang__
 #pragma clang diagnostic push
  #pragma clang diagnostic ignored "-Wextra-semi"
